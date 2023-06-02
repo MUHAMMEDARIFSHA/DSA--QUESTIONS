ee0511fa12ec3d7a726d914d7cad26ae441041c4
MUHAMMED ARIFSHA <muhammedarifsha2001@gmail.com>
2023-06-02 10:51:45 +0530

    june 2

---
+++ leetCode2582.js
@@ -0,0 +1,36 @@
+// There are n people standing in a line labeled from 1 to n. The first person in the line is holding a pillow initially. Every second, the person holding the pillow passes it to the next person standing in the line. Once the pillow reaches the end of the line, the direction changes, and people continue passing the pillow in the opposite direction.
+
+// For example, once the pillow reaches the nth person they pass it to the n - 1th person, then to the n - 2th person and so on.
+// Given the two positive integers n and time, return the index of the person holding the pillow after time seconds.
+
+ 
+
+// Example 1:
+
+// Input: n = 4, time = 5
+// Output: 2
+// Explanation: People pass the pillow in the following way: 1 -> 2 -> 3 -> 4 -> 3 -> 2.
+// Afer five seconds, the pillow is given to the 2nd person.
+
+
+var passThePillow = function(n, time) {
+    const x = Math.floor(time/ (n-1))
+    const remainder = time% (n-1)
+ 
+if(x%2===0){
+    return remainder +1
+}
+return n -remainder
+   
+}
+console.log(passThePillow(n=4,time=5));
+console.log(passThePillow(n=3,time=2));
+console.log(passThePillow(n=18,time=38))
+
+
+
+
+
+
+
+
