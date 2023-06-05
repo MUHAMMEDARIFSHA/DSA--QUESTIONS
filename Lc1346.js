1c5f41ef683945df0d7fb09c15387cc9e7fe68e3
MUHAMMED ARIFSHA <muhammedarifsha2001@gmail.com>
2023-06-05 11:35:45 +0530

    june 5

--- Lc1346.js
+++ Lc1346.js
@@ -12,3 +12,20 @@
 // Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
 
 
+var checkIfExist = function(arr) {
+    for(let i=0; i<arr.length;i++){
+        for(let j=(i+1);j<arr.length;j++){
+            console.log(arr[i] , arr[j]*2)
+            if(arr[i]===(2*arr[j])){
+                return true
+            }
+            if((2*arr[i])===arr[j]){
+                return true
+            }
+        }
+    }
+    return false
+};
+// console.log(checkIfExist([10,2,5,3]))
+ console.log(checkIfExist([7,1,14,11]))
+// console.log(checkIfExist([-2,0,10,-19,4,6,-8]))
