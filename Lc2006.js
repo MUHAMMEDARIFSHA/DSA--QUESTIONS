bd0022149bbde07ea220b34542d1771f1c71f8aa
MUHAMMED ARIFSHA <muhammedarifsha2001@gmail.com>
2023-06-26 09:43:11 +0530

    june 26  Q-29

---
+++ Lc2006.js
@@ -0,0 +1,31 @@
+// Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+// The value of |x| is defined as:
+
+// x if x >= 0.
+// -x if x < 0.
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,2,1], k = 1
+// Output: 4
+// Explanation: The pairs with an absolute difference of 1 are:
+// - [1,2,2,1]
+// - [1,2,2,1]
+// - [1,2,2,1]
+// - [1,2,2,1]
+
+var countKDifference = function(nums, k) {
+    let count =0
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+          if( Math.abs(nums[i]-nums[j])===k){
+              count++
+          }
+        }
+    }
+    return count
+    };
+
+    console.log( countKDifference([1,2,2,1],k=1));
