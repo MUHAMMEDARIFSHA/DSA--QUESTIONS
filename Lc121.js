a0dd6564944239649bfe8e884734b4d2d8ef7ae8
MUHAMMED ARIFSHA <muhammedarifsha2001@gmail.com>
2023-06-30 10:10:11 +0530

    june 30 Q- 35

---
+++ Lc121.js
@@ -0,0 +1,30 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    let minPrice = Infinity;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+};
+console.log(maxProfit( [7,1,5,3,6,4]));
