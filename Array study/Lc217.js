7a28584a98c2d2d010d879c031cf19d9cbfd6a60
MUHAMMED ARIFSHA <muhammedarifsha2001@gmail.com>
2025-03-25 12:32:42 +0530

    .

---
+++ Array study/Lc217.js
@@ -0,0 +1,49 @@
+// 217. Contains Duplicate
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+
+// Output: true
+
+// Explanation:
+
+// The element 1 occurs at the indices 0 and 3.
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+
+// Output: false
+
+// Explanation:
+
+// All elements are distinct.
+
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+// Output: true
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let seen = new Set()
+    for(let i=0;i<nums.length;i++){
+        if(seen.has(nums[i])){
+            return true
+        }
+        seen.add(nums[i])
+    }
+    return false
+};
+
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
