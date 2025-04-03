85ef8238493294f99f80fa6b578ac43fbffbf997
AR <muhammedarifsha2001@gmail..com>
2025-04-03 23:14:54 +0530

    recursiong new questions

---
+++ Recursion/rangeOfNumbers.js
@@ -0,0 +1,15 @@
+// create an array using recursion with given range of numbers 
+
+
+function rangeOfNumber(startRange, endRange){
+    if(startRange > endRange){
+        return []
+    }
+    else{ 
+        let numbers = rangeOfNumber(startRange+1,endRange)
+        numbers.unshift(startRange)
+        return numbers
+    }
+}
+
+console.log(rangeOfNumber(1,4))
