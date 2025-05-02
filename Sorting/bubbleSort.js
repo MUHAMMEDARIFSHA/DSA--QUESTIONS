e2991b474364c463fbc9a5270f456f9a19b403eb
MUHAMMED ARIFSHA <muhammedarifsha2001@gmail.com>
2025-05-02 18:14:13 +0530

    bubble sort

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,20 @@
+function bubbleSort(arr) {
+    const n = arr.length;
+    let swapped;
+  
+    do {
+      swapped = false;
+      for (let i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          // Swap arr[i] and arr[i+1]
+          [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+          swapped = true;
+        }
+      }
+    } while (swapped);
+    return arr;
+  }
+
+  
+
+  console.log(bubbleSort([1,4,2,5,4,7,0]))
